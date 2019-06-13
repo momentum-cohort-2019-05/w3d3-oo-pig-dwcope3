3b0d0c1026312209d52630010d82a6b36c0f752d
dwcope3 <dwcope3@gmail.com>
2019-06-13 00:34:31 -0400

    finally got it working after using pdb for 2 hours

--- pig.py
+++ pig.py
@@ -1,8 +1,8 @@
 import random
 
 def roll_die():
-    return random.randit(1,6)
-    
+    return random.randint(1,6)
+
 class ComputerPlayer:
     """
     computer player class
@@ -24,9 +24,7 @@ class ComputerPlayer:
 class HumanPlayer:
     """
     Human Player Class
-    """
-        
-
+    """ 
 
     def __init__(self):
         self.current_score = 0
@@ -35,7 +33,7 @@ class HumanPlayer:
         self.current_score = 0
 
     def roll(self, number):
-        self.curent_score += number
+        self.current_score += number
 
     def roll_again(self):
         decision = None
@@ -43,35 +41,89 @@ class HumanPlayer:
             decision = input("(R)oll again or (H)old? ").lower()
         return decision[0] == "r"
 
+
 class Game:
     """
     Game class
     """
-
-
     def __init__(self, die, player1, player2):
         self.die = die
         self.players = [player1, player2]
         self._current_player = None
         self.reset_scores()
 
-    def start_game(self):
-        self.print_instructions()
-        play_again = True
-        while play_again:
-            self.run_game()
-            play_again = input("Play Again? (Y/N) ")
-            play_again = play_again.lower().startswith("y")
-
     def reset_scores(self):
         self.scores = [0,0]
         self.round_score = 0
 
     def decide_first_player(self):
+        self._current_player = random.randint(0,1)
+        print(f"Player {self._current_player + 1} starts")
+
+    def get_current_player(self):
+        return self.players[self._current_player]
+
+    def switch_player(self):
+        if self._current_player == 0:
+            self._current_player = 1
+        else:
+            self._current_player = 0
+        print(f"It is player {self._current_player + 1}'s turn.")
+
+    def game_ends(self):
+        return self.scores[0] >= 100 or self.scores[1] >= 100
+
+    def start_game(self):
+        self.welcome()
+        self.reset_scores()
+        self.decide_first_player()
+        
+        while not self.game_ends():
+            self.run_game()
+            if not self.game_ends():
+                self.switch_player()
+
+        # play_again = True
+        # while play_again:
+        #     self.run_game()
+        #     play_again = input("Play Again? (Y/N) ")
+        #     play_again = play_again.lower().startswith("y")
+
+        
+    
+        print(f"Player {self._current_player + 1} Wins!")
+
+    def run_game(self):
+        turn_score = 0
+        player = self.get_current_player()
+        player.start_of_game()
+       
+        roll = self.die()
+        player.roll(roll)
+        print(f"Roll: {roll}")
+
+        while roll != 1:
+            turn_score += roll
+            if not player.roll_again():
+                break
+            roll = self.die()
+            player.roll(roll)
+            print(f"Roll: {roll}")
+
+        if roll == 1:
+            turn_score = 0
+
+        print (f"Turn score: {turn_score}")
+        self.scores[self._current_player] += turn_score
+        print(f"Current score for player {self._current_player + 1}: {self.scores[self._current_player]}")
+
+    def welcome(self):
+        print("welcome")
+
+# breakpoint()
+if __name__ == "__main__":
+    game = Game(roll_die, HumanPlayer(), ComputerPlayer())
+    game.start_game()
 
 
     
-
-    
-
-
